44ea9035860aa610a747d9042a6928b6cc8a9fac
Filippo Tessarotto <zoeslam@gmail.com>
2021-11-08 14:27:48 +0100

    Test in sub-namespace

--- test/V1EncryptorTest.php
+++ test/V1EncryptorTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace SlamSymmetricEncryptionTest;
+namespace SlamSymmetricEncryption\Test;
 
 use PHPUnit\Framework\TestCase;
 use SlamSymmetricEncryption\EncryptorException;
